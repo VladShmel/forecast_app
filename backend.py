4f2ac7d00f92677e59b560abbf3221eee919c6e7
Uladzislau_Melikhau <vlad_melichov2014@mail.ru>
2023-07-12 18:20:16 +0300

    Add "Sky" option

--- backend.py
+++ backend.py
@@ -1,19 +1,15 @@
 import requests
 
-API_KEY = "2d6a3b35823e3f8ad3cba99c9268c4f4"
+API_KEY = "3d6a3b35823e3f8ad3cba99c9268c4f5"
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__ == "__main__":
